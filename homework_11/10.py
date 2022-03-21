533b6946b902b4b5325b4f0e2d2fab3e88d14a7b
Your Name <you@example.com>
2022-03-21 11:40:11 +0300

    11

---
+++ homework_11/10.py
@@ -0,0 +1,6 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+k = int(input())
+text = input()
+for i in range(len(text)):
+
+print(text)
