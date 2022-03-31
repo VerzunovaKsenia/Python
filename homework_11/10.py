55bf21637f60a8934b9f09e2a951b9eb50500792
VerzunovaKsenia <verzunova.02@mail.ru>
2022-03-31 13:06:36 +0300

    11

--- homework_11/10.py
+++ homework_11/10.py
@@ -1,6 +1,13 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-k = int(input())
-text = input()
-for i in range(len(text)):
-
-print(text)
+message = input('Введите сообщение: ')
+k = int(input('Введите сдвиг: '))
+encrypter = {}
+for i, elem in enumerate(alphabet):
+    encrypter[elem] = alphabet[(i - k) % len(alphabet)]
+encrypted_message = ''
+for i in message:
+    if encrypter.get(i):
+        encrypted_message += encrypter[i]
+    else:
+        encrypted_message += i
+print('Зашифрованное сообщение:', encrypted_message)
